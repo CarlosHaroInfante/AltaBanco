52afeccdc02a4eb17dbdf295bd2b108e68272b21
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2023-11-21 14:11:07 +0100

    Add files via upload

---
+++ DarDeAlta/Servicios/CuentaImplementacion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DarDeAlta.Dtos;
+
+namespace DarDeAlta.Servicios
+{
+    internal class CuentaImplementacion : CuentaInterfaz
+    {
+        public void nuevaCuenta(List<CuentasDto> cuentaAntigua)
+        {
+            CuentasDto cuentaNueva = new CuentasDto();
+            cuentaAntigua.Add(cuentaNueva);
+
+            Console.WriteLine("Datos de la Cuenta");
+
+            Console.WriteLine("ID: ");
+            cuentaNueva.IdCuenta = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("ISBAN: ");
+            cuentaNueva.ISBANCuenta = Console.ReadLine();
+
+            Console.WriteLine("Switch: ");
+            cuentaNueva.CodigoSwitch = Console.ReadLine();
+
+            Console.WriteLine("Fecha de Alta: ");
+            cuentaNueva.FechaCuentaAlta = Console.ReadLine();
+
+
+            
+        }
+
+    }
+}
---
+++ DarDeAlta/Servicios/NuevoImplementacion.cs
@@ -0,0 +1,65 @@
+﻿using DarDeAlta.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarDeAlta.Servicios
+{
+    internal class NuevoImplementacion : NuevoInterfaz
+    {
+
+
+        public void darAltaCliente(List<ClientesDto> listaAntigua) { 
+       
+
+            ClientesDto clienteNuevo = new ClientesDto();
+             listaAntigua.Add(clienteNuevo);
+        
+        
+        /// <summary>
+        /// Implementación donde se pondrán los datos del usuario y lo añadirá a la lista.
+        /// 171123 - chi
+        /// </summary>
+       
+            
+
+            Console.WriteLine("Datos del usuario");
+
+            Console.WriteLine("ID: ");
+            clienteNuevo.IdCliente = Convert.ToInt64(Console.ReadLine());
+            long idClient = clienteNuevo.IdCliente;
+
+            Console.WriteLine("Nombre: ");
+            clienteNuevo.NombreCliente = Console.ReadLine();
+
+            Console.WriteLine("Apellidos: ");
+            clienteNuevo.ApellidosCliente = Console.ReadLine();
+
+            Console.WriteLine("DNI: ");
+            clienteNuevo.DniCliente = Console.ReadLine();
+
+            Console.WriteLine("Fecha de nacimiento: ");
+            clienteNuevo.FchNacimientoCliente = Console.ReadLine();
+
+            Console.WriteLine("email: ");
+            clienteNuevo.EmailCliente = Console.ReadLine();
+
+            Console.WriteLine("Tlf: ");
+            clienteNuevo.TlfCliente = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fecha de Alta: ");
+            clienteNuevo.FchAltaCliente = Console.ReadLine();
+
+           
+
+        }
+
+
+
+    }
+}
